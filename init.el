6070e5fcc59ec64fb6a524ceef29287e2ef447df
Shantanu Balse <infoshantanu02@gmail.com>
2019-12-28 09:59:09 +0530

    Removed the key binding. Doesnt seem to be working

--- init.el
+++ init.el
@@ -56,12 +56,7 @@
 ;(split-window-horizontally)
 
 ;--------- Keybindings -----------
-(global-set-key (kbd "C-.") #'other-window)
-(global-set-key (kbd "C-,") #'prev-window)
 
-(defun prev-window ()
-  (interactive)
-  (other-window -1))
 ;--------- Keybindings -----------
 
 (custom-set-variables
